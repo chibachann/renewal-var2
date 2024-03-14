8662ff667913f4c1ea6d3b05dd163defa253f16b
中野 智貴 <nakano@r-unit.co.jp>
2024-03-14 10:33:27 +0900

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,25 @@
 /**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+require('dotenv').config({
+  path: `.env`,
+});
+
+/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
   siteMetadata: {
-    title: `test`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `デジタルクリエイティブ株式会社`,
+    description: `デジタルクリエイティブ株式会社は、八王子市を拠点とした、ITコンサルティング・Webサイト制作・Webシステム開発・Webマーケティングを行う会社です。`,
+    siteUrl: `https://digitalcreative.co.jp`,
+    companyName: `デジタルクリエイティブ株式会社`,
   },
   plugins: [
     `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -17,5 +27,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
   ],
-}
+};
